5a69f55e48ef217beae136f1c36f14ef371d7554
Matt Traudt <sirmatt@ksu.edu>
2019-06-17 10:13:48 -0400

    Accept start/stop/reload/restart commands

--- run-relays.sh
+++ run-relays.sh
@@ -2,8 +2,40 @@
 set -eu
 
 tor_bin=$(which tor)
+commands=( start stop reload restart )
 
-for torrc in $(pwd)/*/torrc; do
-	echo $torrc
-	sudo $tor_bin -f $torrc --quiet &
-done
+function usage {
+	echo "valid commands: ${commands[@]}"
+}
+
+[[ "${1:-}" == "" ]] && usage && exit 1
+[[ " ${commands[@]} " != *" $1 "* ]] && usage && exit 1
+
+function start_fn {
+	for torrc in $(pwd)/*/torrc; do
+		echo Starting tor with $torrc
+		sudo $tor_bin -f $torrc --quiet &
+	done
+}
+
+function stop_fn {
+	echo SIGINT PIDs: $(cat */tor.pid)
+	cat */tor.pid | xargs kill -INT
+}
+
+function reload_fn {
+	echo SIGHUP PIDs: $(cat */tor.pid)
+	cat */tor.pid | xargs kill -HUP
+}
+
+function restart_fn {
+	stop_fn
+	sleep 5
+	start_fn
+}
+
+command="$1"
+[[ "$command" == "start" ]] && start_fn
+[[ "$command" == "stop" ]] && stop_fn
+[[ "$command" == "reload" ]] && reload_fn
+[[ "$command" == "restart" ]] && restart_fn
